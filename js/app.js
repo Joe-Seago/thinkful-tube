22e68109b67bfac7b62d281f9d398ea113c51a56
Joe Seago <joe.seago@gmail.com>
2016-06-22 14:03:29 -0600

    added iframe of video at bottom of thumbnails

--- js/app.js
+++ js/app.js
@@ -12,8 +12,7 @@ $(document).ready(function () {
       }
     }).done(function (results) {
       $.each(results.items, function (index, value) {
-        $('.search-results').append('<li><a href="https://www.youtube.com/watch?v=' +
-          value.id.videoId + '"><img src=' + value.snippet.thumbnails.medium.url +
+        $('.search-results').append('<li id="' + value.id.videoId + '"><a href="#"><img src=' + value.snippet.thumbnails.medium.url +
           '></a></li>');
       });
     });
@@ -26,5 +25,14 @@ $(document).ready(function () {
 
     var searchTerm = $('#text-input').val();
     searchYoutube(searchTerm);
+});
+
+  $('.search-results').on('click', 'li', function() {
+ 	$('.lightbox-container').empty();
+ 	$('.lightbox-container').append('<iframe width="560" height="315" src="https://youtube.com/embed/' + $(this).attr('id') + '" frameborder="0" allowfullscreen></iframe>');
+  	// console.log($(this).attr('href'));
+
+  	//<a class="thumbnails" href="https://www.youtube.com/watch?v=' +
+    //      value.id.videoId + '">
   });
 });
